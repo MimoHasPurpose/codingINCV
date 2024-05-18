90b490ec62b42d66ccf07bafe050f5e955f3ef7d
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-05-18 18:35:25 +0530

    updated the code with numpy

--- c1BasicFunction.py
+++ c1BasicFunction.py
@@ -1,11 +1,11 @@
 import cv2
-
+import numpy as np
 img=cv2.imread("Resources/default.png")
-
+kernel=np.ones((5,5),np.unit8)
 imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur=cv2.GaussianBlur(imgGray,(7,7),0)
 imgCanny=cv2.Canny(img,100,100)
-
+imgDialation=cv2.dilate(imgCanny,)
 
 cv2.imshow("Gray image", imgGray)
 cv2.imshow("Blur Image",imgBlur)
