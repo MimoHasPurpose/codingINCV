7fd6683c7743dac1dbfa272d536fdc8677d9e3fb
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-05-19 15:13:46 +0530

    updated the code

--- First/chapter8.py
+++ First/chapter8.py
@@ -1,1 +1,9 @@
 #color detection
+import cv2
+import numpy as np
+path="../Resources/lambo.png"
+img=cv2.imread(path)
+imgHSV=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+hor=np.hstack((img,imgHSV))
+cv2.imshow("2 images: ",hor)
+cv2.waitKey(0)
